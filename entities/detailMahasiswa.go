b70fb69f02f2ad40600814a963eac20b1008410f
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-06-17 12:31:34 +0700

    Membuat tampilan dan fungsi detail

---
+++ entities/detailMahasiswa.go
@@ -0,0 +1,12 @@
+package entities
+
+type MahasiswaDetail struct {
+    Id_mahasiswa int
+    Nama         string
+    Kelas        string
+    Matematika   int
+    Kimia        int
+    Fisika       int
+    Biologi      int
+    TotalNilai   int
+}
